c86808ba5cb9b514e02d42412e0e6c4deb4d66ff
Chunkai Yang <matYang@users.noreply.github.com>
2026-01-29 05:58:48 -0800

    Port over OZ 5.3.0 bump (#10)
    * port over existing latest to 1_6_1
    
    * Bump OZ 5.3.0 (#21)
    
    * bump oz 5.3.0
    
    * add OZ contracts to 1.0.0 folder
    
    * word nit
    
    * snapshot
    
    * move 1_0_0 to 1_6_1
    
    * remove metadata files from latest
    
    * accept metadata flag
    
    * refresh wrappers
    
    * new snapshot

--- chains/evm/gobindings/go_generate.go
+++ chains/evm/gobindings/go_generate.go
@@ -4,3 +4,5 @@ package gobindings
 //go:generate go run generation/generate/wrap.go default BurnMintWithExternalMinterTokenPool burn_mint_with_external_minter_token_pool latest
 //go:generate go run generation/generate/wrap.go default HybridWithExternalMinterTokenPool hybrid_with_external_minter_token_pool latest
 //go:generate go run generation/generate/wrap.go default Stablecoin usd_stablecoin latest
+//go:generate go run generation/generate/wrap.go default TransparentUpgradeableProxy transparent_upgradeable_proxy latest
+//go:generate go run generation/generate/wrap.go default ProxyAdmin proxy_admin latest
